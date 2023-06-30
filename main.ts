c86a31098609dc8e19335b0b445839f78a974352
mostertjc <102650119+mostertjc@users.noreply.github.com>
2023-06-30 13:27:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
-let Counter = 0
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
     Counter += 1
+    basic.showNumber(Counter)
+})
+let Counter = 0
+Counter = 0
+basic.forever(function () {
+	
 })
