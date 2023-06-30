2b1bb4c8515718aefca04becab3fb6ff8378d644
mostertjc <102650119+mostertjc@users.noreply.github.com>
2023-06-30 13:33:38 +0200

    Count up and count down

--- main.ts
+++ main.ts
@@ -2,6 +2,10 @@ input.onButtonPressed(Button.A, function () {
     Counter += 1
     basic.showNumber(Counter)
 })
+input.onButtonPressed(Button.B, function () {
+    Counter += -1
+    basic.showNumber(Counter)
+})
 let Counter = 0
 Counter = 0
 basic.forever(function () {
